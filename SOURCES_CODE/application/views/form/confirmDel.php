a11109dab09fb3f7e6e49be327dd34cb2a9904d6
jedsadakorn sirikunpan <aun_never_die@hotmail.com>
2014-10-29 02:42:06 +0700

    Revert "เพิ่มการเรียกดู เด็กในพื้นที่การดูแลของผู้ล็อกอิน"
    This reverts commit 7e2a127ae9f65b84143d8b2901b4a7db30571b52.

--- SOURCES_CODE/application/views/form/confirmDel.php
+++
@@ -1,6 +0,0 @@
-<body style="text-align: center"><p>คุณต้องการลบข้อมูล หรือไม่</p>
-<p>
-  <input type="button" name="button" id="button" value="ยืนยันการลบ">
-  &nbsp;&nbsp;&nbsp;
-  <input type="button" name="button2" id="button2" value="ยกเลิก">
-</p>
